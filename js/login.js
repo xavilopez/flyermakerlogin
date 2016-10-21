5e58201e6d225dc393358e9497bf794e146dcd88
xavilopez <xavi_lopez011@hotmail.com>
2016-10-21 16:14:47 -0300

    first commit

---
+++ js/login.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+  $("#inputUser").val('');
+  $("#inputPass").val('');
+
+
+  $("#modalUser").on('click', function(){
+      $("#inputUser").focus();
+  });
+  $("#modalPass").on('click', function(){
+      $("#inputPass").focus();
+  });
+
+  $("#btnLogin").on('click', function(){
+      login();
+  });
+
+
+});
+
+
+function login() {
+  var user = $("#inputUser").val(),
+      pass = $("#inputPass").val();
+  switch(user,pass){
+
+    case 'admin', '123':
+      console.log("entro admin");
+      break;
+
+    case 'martin', 'martin123':
+      console.log("entro martin");
+      break;
+    default:
+      console.log("default");
+  };
+
+}
