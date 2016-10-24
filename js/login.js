2b2bc1231bba72d9f825eb06f37e708bb4dc4df3
xavilopez <xavi_lopez011@hotmail.com>
2016-10-24 09:25:38 -0300

    login va al main

--- js/login.js
+++ js/login.js
@@ -2,18 +2,23 @@ $(document).ready(function(){
   $("#inputUser").val('');
   $("#inputPass").val('');
 
-
-  $("#modalUser").on('click', function(){
-      $("#inputUser").focus();
-  });
-  $("#modalPass").on('click', function(){
-      $("#inputPass").focus();
-  });
-
   $("#btnLogin").on('click', function(){
       login();
   });
 
+  $("#inputUser").keypress(function(e){
+    var keycode = (e.keyCode ? e.keyCode : e.which);
+    if(keycode ==13){
+      $("#inputPass").focus();
+    }
+  });
+
+  $("#inputPass").keypress(function(e){
+    var keycode = (e.keyCode ? e.keyCode : e.which);
+    if(keycode ==13){
+      login();
+    }
+  });
 
 });
 
@@ -25,15 +30,13 @@ function login() {
 
     case 'admin', '123':
       console.log("entro admin");
-      window.location.href='https://xavilopez.github.io/Flyer1/';
-      break;
-
-    case 'martin', 'martin123':
-      console.log("entro martin");
-      window.location.href='https://xavilopez.github.io/Flyer2/';
+      window.location.href='https://xavilopez.github.io/flyermakermain/';
       break;
     default:
       console.log("default");
+      $("#inputUser").val('');
+      $("#inputPass").val('');
+      $("#inputUser").focus();
   };
 
 }
